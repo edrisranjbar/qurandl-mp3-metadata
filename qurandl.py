3e701ac3c4d0501e77634911e81228d4cd2627a3
edris ranjbar <edris.qeshm2@gmail.com>
2020-02-12 23:02:53 +0330

    convert space in qari name to dash for path

--- qurandl.py
+++ qurandl.py
@@ -7,15 +7,15 @@ for i in range(1,115):
         i = str(i)
         i = i.zfill(3)
         # Renaming file name
-        ## TODO: INSTEAD OF SPACE IN QARI NAME WE SHOULD HAVE DASH.
-        path = QARI_NAME + "-" + i + ".mp3"
+        ## INSTEAD OF SPACE IN QARI NAME WE SHOULD HAVE DASH.
+        QARI = QARI.replace(" ","-")
+        path = QARI + "-" + i + ".mp3"
         os.rename(i+".mp3",path)
         # Setting Metadata
         mp3.load(path)
         mp3.tag.artist(QARI_NAME)
         mp3.tag.album("Qurandl")
-        title = ""
-        mp3.tag.title(title)
+        mp3.tag.title(title[i])
         mp3.tag.track_num = i
         mp3.tag.images.set(3, open('cover.jpg','rb').read(),'image/jpeg')
         mp3.tag.save()
