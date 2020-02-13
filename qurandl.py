4a977a78fae5b39e62a3159577a83a2d81f8f73c
edris ranjbar <edris.qeshm2@gmail.com>
2020-02-13 22:36:03 +0330

    remove previous metadata

--- qurandl.py
+++ qurandl.py
@@ -1,6 +1,9 @@
 import os
-import eyed3 as mp3
+import eyed3
 import title
+import sys
+import codecs
+from mutagen.mp3 import MP3
 QARI_NAME = input("Enter qari name: ")
 for i in range(1,115):
     try:
@@ -8,16 +11,22 @@ for i in range(1,115):
         i = i.zfill(3)
         # Renaming file name
         ## INSTEAD OF SPACE IN QARI NAME WE SHOULD HAVE DASH.
-        QARI = QARI.replace(" ","-")
+        QARI = QARI_NAME.replace(" ","-")
         path = QARI + "-" + i + ".mp3"
         os.rename(i+".mp3",path)
+        # Rmove previous metadata
+        mp3 = MP3(path)
+        try:
+           mp3.delete()
+           mp3.save()
         # Setting Metadata
-        mp3.load(path)
-        mp3.tag.artist(QARI_NAME)
-        mp3.tag.album("Qurandl")
-        mp3.tag.title(title[i])
-        mp3.tag.track_num = i
-        mp3.tag.images.set(3, open('cover.jpg','rb').read(),'image/jpeg')
-        mp3.tag.save()
+        eyed3.load(path)
+        eyed3.delete()
+        eyed3.tag.artist(QARI_NAME)
+        eyed3.tag.album("Qurandl")
+        eyed3.tag.title(title[i])
+        eyed3.tag.track_num = i
+        eyed3.tag.images.set(3, open('cover.jpg','rb').read(),'image/jpeg')
+        eyed3.tag.save()
     except:
         continue
