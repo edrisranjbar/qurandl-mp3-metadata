1f58084058fcc89a0bf131a8f2d573e72431f99b
edris ranjbar <edris.qeshm2@gmail.com>
2020-03-09 18:30:40 +0330

    display menu && Qari name capitalization && and some todo

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ Set metadata and renaming quran files
 
 ## TODO:
 - [x] Change file name
-- [ ] Reset previous Meta Data
+- [x] Reset previous Meta Data
 - [x] Set meta Data
+- [x] Capitalize qari name
+- [x] Display a menu to select an item
+- [ ] Move ```.mp3``` files to a subfolder with the name of Qari its self
+- [ ] Zip folder and finally delete the folder
+- [ ] Logging
--- qurandl.py
+++ qurandl.py
@@ -1,13 +1,50 @@
-"""
-    REMOVE PREVIOUS METADATA AND EMBED
-    OUR META DATA TO MP3 FILES.
-"""
 import os
 import eyed3 as the_mp3
 from mutagen.mp3 import MP3
 import title
-if __name__ == "__main__":
-    QARI_NAME = input("Enter qari name: ")
+from selenium import webdriver
+import wget
+
+def display_menu():
+    selected = input("""WHICH ONE? (1-2)
+    1) DOWNLOAD MP3 QURAN FILES IN A WEBPAGE
+    2) MODIFY NAME AND METADATA
+    PLEASE SPECIFIE WITH A NUMBER: 
+    """)
+    if selected == "1":
+        url = input("Type your url: ")
+        download_all_mp3_files(url)
+
+    elif selected == "2":
+        modify_metatag()
+
+    else:
+        print("PLEASE CHOOSE AGAIN!")
+        display_menu()
+
+def download_all_mp3_files(url):
+    """
+        DOWNLOAD ALL MP3 FILE IN A WEBPAGE
+    """
+    browser = webdriver.Chrome(executable_path='{}/chromedriver'.format(os.curdir))
+    browser.get(url)
+    links = browser.find_elements_by_link_text('اضغط هنا للتحمیل')
+    for link in links:
+        try:
+            href = link.get_attribute("href")
+            print("DOWNLOADING {}".format(href))
+            wget.download(href)
+        except:
+            print("Can't download {}".format(link))
+    print("Download Completed!")
+    return True
+
+def modify_metatag():
+    """
+    REMOVE PREVIOUS METADATA AND EMBED
+    OUR META DATA TO MP3 FILES.
+    """
+    QARI_NAME = input("Enter qari name: ").title()
     for i in range(1, 115):
         try:
             i = str(i)
@@ -32,3 +69,8 @@ if __name__ == "__main__":
             the_mp3.tag.save()
         except:
             continue
+    return True
+
+
+if __name__ == "__main__":
+    display_menu()
